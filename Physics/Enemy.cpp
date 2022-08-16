f74c209eb788ae839551de81795b6e0bb45756eb
abzhikai <abzhikai@gmail.com>
2022-08-16 10:54:41 +0800

    Add spawn for enemy

--- Physics/Enemy.cpp
+++ Physics/Enemy.cpp
@@ -2,6 +2,8 @@
 
 Enemy::Enemy()
 {
+	x = 0;
+	y = 0;
 }
 
 Enemy::~Enemy()
@@ -10,9 +12,26 @@ Enemy::~Enemy()
 
 bool Enemy::Init(void)
 {
+
 	return false;
 }
 
 void Enemy::Update(const double dElapsedTime)
 {
+}
+
+void Enemy::setSpawn()
+{
+	int spawnLocation = Math::RandIntMinMax(0, 3);
+	switch (spawnLocation)
+	{
+	case NORTH:
+		
+	case EAST:
+
+	case SOUTH:
+
+	case WEST:
+
+	}
 }
