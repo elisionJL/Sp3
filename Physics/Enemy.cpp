33b3fd9e51520d9aa50c155ea24f368c6d780826
abzhikai <abzhikai@gmail.com>
2022-08-16 10:55:33 +0800

    enemy stoopid error

--- Physics/Enemy.cpp
+++ Physics/Enemy.cpp
@@ -23,15 +23,22 @@ void Enemy::Update(const double dElapsedTime)
 void Enemy::setSpawn()
 {
 	int spawnLocation = Math::RandIntMinMax(0, 3);
+	float w, h;
+	w = Application::GetWindowWidth();
+	h = Application::GetWindowHeight();
 	switch (spawnLocation)
 	{
 	case NORTH:
-		
+		x = Math::RandFloatMinMax(0, w);
+		y = h + (h / 4);
 	case EAST:
-
+		x = w + (w / 4);
+		y = Math::RandFloatMinMax(0, h);
 	case SOUTH:
-
+		x = Math::RandFloatMinMax(0, w);
+		y = 0 - (h / 4);
 	case WEST:
-
+		x = 0 - (w / 4);
+		y = Math::RandFloatMinMax(0, h);
 	}
 }
